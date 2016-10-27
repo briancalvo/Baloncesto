ff1ef4cc79ae7e6191819416c554de1220deead7
Brian <brian.calvo.aragon@gmail.com>
2016-10-27 17:22:52 +0200

    Subido nuevo proyecto remasterizado para la Clase.

--- src/main/java/com/briancalvo/BaloncestoApplication.java
+++ src/main/java/com/briancalvo/BaloncestoApplication.java
@@ -12,7 +12,6 @@ public class BaloncestoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(BaloncestoApplication.class, args);
 		PlayerService playerService=context.getBean(PlayerService.class);
-
 		playerService.testPlayers();
 
 	}
