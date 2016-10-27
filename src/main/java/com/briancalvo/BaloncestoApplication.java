f7cf18ff14fcb620eefcf1a757ccfed1a5b5c4b9
Brian Calvo <brian.calvo.aragon@gmail.com>
2016-10-27 17:26:36 +0200

    Revert "Subido nuevo proyecto remasterizado para la Clase."
    This reverts commit ff1ef4cc79ae7e6191819416c554de1220deead7.

--- src/main/java/com/briancalvo/BaloncestoApplication.java
+++ src/main/java/com/briancalvo/BaloncestoApplication.java
@@ -12,6 +12,7 @@ public class BaloncestoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(BaloncestoApplication.class, args);
 		PlayerService playerService=context.getBean(PlayerService.class);
+
 		playerService.testPlayers();
 
 	}
